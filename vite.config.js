c4bd30d138dadd0f8b799ff503942448d50362b7
cheatcode12 <123173764+cheatcode12@users.noreply.github.com>
2024-05-23 05:06:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		port: 8080 // Hier den gewünschten Port angeben
-	  },
 	plugins: [sveltekit()]
-	
 });
